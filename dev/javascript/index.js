7d4b2d69f92892a84022ee983475e530bbfa1b3d
logty3 <logty3@bk.ru>
2020-06-25 21:39:05 +0300

    Mask number form

--- dev/javascript/index.js
+++ dev/javascript/index.js
@@ -3,50 +3,38 @@ import "../stylesheets/index.scss";
 import "bootstrap";
 
 const consultationForm = document.getElementById("consultation_form");
-
-const consultationNumber = document.getElementById("consultation-number");
-/*
-consultationNumber.onkeypress = (e) => {
-  e.preventDefault();
-  console.log(e.key);
-  if (/\d/.exec(e.key)) {
-    consultationNumber.value = consultationNumber.value.replace("_", e.key);
-  }
-};*/
-consultationNumber.onkeydown = (e) => {
-  e.preventDefault();
-  if (e.code == "Delete" || e.code == "Backspace") {
-    if (consultationNumber.value.indexOf("_") != 2) {
-      consultationNumber.value = consultationNumber.value
-        .split("")
-        .reverse()
-        .join("")
-        .replace(/\d/, "_")
-        .split("")
-        .reverse()
-        .join("");
+if (consultationForm) {
+  const consultationNumber = document.getElementById("consultation-number");
+  consultationNumber.onkeydown = (e) => {
+    e.preventDefault();
+    if (e.code == "Delete" || e.code == "Backspace") {
+      if (consultationNumber.value.indexOf("_") != 2) {
+        consultationNumber.value = consultationNumber.value
+          .split("")
+          .reverse()
+          .join("")
+          .replace(/\d/, "_")
+          .split("")
+          .reverse()
+          .join("");
+      }
     }
-  }
+    if (/\d/.exec(e.key)) {
+      consultationNumber.value = consultationNumber.value.replace("_", e.key);
+    }
+  };
+  consultationNumber.onpaste = (e) => {
+    e.preventDefault();
+  };
 
-  if (/\d/.exec(e.key)) {
-    consultationNumber.value = consultationNumber.value.replace("_", e.key);
-  }
-};
-consultationNumber.onpaste = (e) => {
-  e.preventDefault();
-};
+  consultationNumber.on;
 
-consultationNumber.on;
-
-consultationForm &&
-  (consultationForm.onsubmit = async (e) => {
+  consultationForm.onsubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(consultationForm);
     const body = {};
     for (let [name, value] of formData) body[name] = value;
     console.log(body);
-    consultationForm.classList.add("is-invalid");
-    consultation - number;
 
     return;
     const response = await fetch("/consultation", {
@@ -57,4 +45,5 @@ consultationForm &&
 
     const data = await response.json();
     console.log(data);
-  });
+  };
+}
